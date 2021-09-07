6bf987ac9afdd71ad1044fd3d7d8976d749c872f
superpg-WA <673843192@qq.com>
2021-09-07 21:05:25 +0800

    modify settings

--- fabfile.py
+++ fabfile.py
@@ -8,11 +8,11 @@ def _get_github_auth_responders():
     返回 GitHub 用户名密码自动填充器
     """
     username_responder = Responder(
-        pattern="Username for 'http://github.com':",
+        pattern="Username for 'https://github.com':",
         response='{}\n'.format(github_username)
     )
     password_responder = Responder(
-        pattern="Password for 'http://{}@github.com':".format(github_username),
+        pattern="Password for 'https://{}@github.com':".format(github_username),
         response='{}\n'.format(github_password)
     )
     return [username_responder, password_responder]
